b444bc6d23975094bcd173e9acb936e84867092e
Jordan Powell <jordan@jpdesigning.com>
2025-02-05 17:47:19 -0500

    update schema.json files

--- libs/internal-plugin/src/generators/update-scope-schema/generator.ts
+++ libs/internal-plugin/src/generators/update-scope-schema/generator.ts
@@ -1,17 +1,44 @@
 import {
-  readNxJson,
+  formatFiles,
+  getProjects,
+  ProjectConfiguration,
   Tree,
-  updateNxJson
+  updateJson
 } from '@nx/devkit';
-import { UpdateScopeSchemaGeneratorSchema } from './schema';
 
 export async function updateScopeSchemaGenerator(
-  tree: Tree,
-  options: UpdateScopeSchemaGeneratorSchema
+  tree: Tree
 ) {
-  const nxJson = readNxJson(tree);
-  nxJson.defaultProject = 'movies-app';
-  updateNxJson(tree, nxJson);
+  const projects = getProjects(tree);
+
+  const scopes = getScopes(projects);
+
+  updateJson(tree, 'libs/internal-plugin/src/generators/util-lib/schema.json',
+    (schemaJson) => {
+      schemaJson.properties.directory['x-prompt'].options = scopes
+      schemaJson.properties.directory.enums = scopes;
+      return schemaJson
+    }
+  )
+
+  await formatFiles(tree);
+}
+
+
+function getScopes(projectMap: Map<string, ProjectConfiguration>) {
+  const allScopes: string[] = Array.from(projectMap.values())
+    .map((project) => {
+      if (project.tags) {
+        const scopes = project.tags.filter((tag: string) => tag.startsWith('scope:'));
+        return scopes;
+      }
+      return []
+    })
+    .reduce((acc, tags) => [...acc, ...tags], [])
+    .map((scope: string) => scope.slice(6));
+
+  // remove duplicates
+  return Array.from(new Set(allScopes));
 }
 
 export default updateScopeSchemaGenerator;
